58a2a2303d155ffb6eeb63282d4259cba4942153
GAGANDEEP SINGH <ip.gagan@gmail.com>
2024-05-19 01:08:47 +0530

    Initial Commit

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="text-center p-6 bg-gray-200">
+        <a className="text-gray-600 font-semibold" href="/">
+          Disease Detection
+        </a>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
